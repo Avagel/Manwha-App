420d1d5de2030f288f63ffb5c5409f93a708b968
Akanume Iruoghene <akanumeiruoghene@gmail.com>
2025-10-01 02:12:59 +0100

    changed env stuff

--- Backend/redisClient.js
+++ Backend/redisClient.js
@@ -1,15 +1,15 @@
 // redisClient.js
 const { createClient } = require("redis");
-REDIS_URI = {
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-  },
-};
+// REDIS_URI = {
+//   username: process.env.REDIS_USERNAME,
+//   password: process.env.REDIS_PASSWORD,
+//   socket: {
+//     host: process.env.REDIS_HOST,
+//     port: process.env.REDIS_PORT,
+//   },
+// };
 
-const redisClient = createClient(REDIS_URI);
+const redisClient = createClient(process.env.REDIS_URL);
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
