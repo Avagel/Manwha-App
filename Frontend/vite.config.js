edbd34c8684a18e329c5fdd5ee39344060d7d0ad
Akanume Iruoghene <akanumeiruoghene@gmail.com>
2025-10-01 08:47:39 +0100

    push

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    historyApiFallback: true,
-  },
-  // Optional: Set base path if deploying to subdirectory
-  base: "./",
-});
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
+})
